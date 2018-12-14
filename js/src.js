3da8cfa6c20ed50ac457d1d7ac3270232a91927e
derun <derun.va@gmail.com>
2018-12-14 20:36:42 +0200

    allmost done

--- js/src.js
+++ js/src.js
@@ -1,12 +1,22 @@
-console.log(document)
-var page = document.querySelector('.page');
-console.log(page)
-var pageWrapper = page.querySelector('.wrapper');
-console.dir(page)
-console.dir(pageWrapper)
-var h1 = page.offsetHeight
-var h2 = pageWrapper.offsetHeight
-console.log( h1, h2)
-if(h1 < h2){
-  page.classList.add('test')
-}
+// alert('hello')
+var menuOpener = document.querySelector('.menu-opener');
+var menu = document.querySelector('.menu');
+console.log(menu);
+console.dir(menuOpener);
+menuOpener.addEventListener('click', function(){
+  menu.classList.toggle('is-active');
+  menuOpener.classList.toggle('is-active');
+  a = Math.floor(Math.random() * 256);
+
+  b = Math.floor(Math.random() * 256);
+
+  c = Math.floor(Math.random() * 256);
+
+  color = 'rgb('+a+','+b+','+c+')'
+
+
+
+
+  menuOpener.style.backgroundColor = color
+
+})
